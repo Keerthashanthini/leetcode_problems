8ca7723e29284681244df28d44fcc4fca5b1a0a6
Keerthashanthini <151423400+Keerthashanthini@users.noreply.github.com>
2025-09-29 10:41:05 +0530

    Create DI String Match.java

---
+++ DI /DI String Match.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n=s.length();
+        int a=0;
+        int b=n;
+        int[]arr=new int[n+1];
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='I'){
+                arr[i]=a++;
+            }
+            else{
+                arr[i]=b--;
+            }
+        }
+        arr[n]=a;
+        return arr;
+    }
+}
